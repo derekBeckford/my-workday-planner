f480e81260316e79b34a374651247f5414b3c51a
Derek Beckford <dbeck1207@icloud.com>
2021-06-13 17:15:26 -0500

    final logic to local storage and color

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # my-workday-planner
+
--- assets/logic.js
+++ assets/logic.js
@@ -1,21 +1,39 @@
-var todayDate = moment().format("MMM Do YY");
+var todayDate = moment().format("LLL");
+var hour = moment(moment().format("hh A"), "hh a");
 
-var currentTime = moment().format("LTS");
+$("#currentDay").append(todayDate);
 
+for (i = 0; i < 10; i++) {
+  var description = $($(".description")[i]);
+  var hourEl = moment($(".hour")[i].innerHTML.trim(), "hh a");
+  var diff = hour.diff(hourEl);
 
-$(document).ready(function() {
-    $(".saveBtn").on("click", function(){
-        var description = $(".description").val;
+  if (diff === 0) {
+    description.addClass("present");
+  } else if (diff < 0) {
+    description.addClass("future");
+  } else if (diff > 0) {
+    description.addClass("past");
+  }
+}
 
-        console.log(description)
+$(".saveBtn").on("click", function () {
+  var input = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
 
-
-        var time = $(".hour").get[0];
-
-        console.log(time);
-
-        localStorage.setItem(time, "userInput")
-
-    })
+  if (input === "") {
+    alert("You must enter something before saving!");
+  } else {
+    localStorage.setItem(time, input);
+  }
 });
 
+window.onload = function () {
+  for (i = 0; i < 10; i++) {
+    var timeBlock = $(".time-block")[i];
+    var timeBlockId = $(timeBlock).attr("id");
+    var description = $(timeBlock).find(".description");
+    var timeBlockReturn = localStorage.getItem(timeBlockId);
+    description.val(timeBlockReturn);
+  }
+};
